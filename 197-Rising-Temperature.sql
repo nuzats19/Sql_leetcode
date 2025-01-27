74a780e6aa38f9a2d2d7c2fac659c04af0fd9aa1
nuzats19 <126117403+nuzats19@users.noreply.github.com>
2025-01-27 11:17:55 -0500

    [197] [Time Beats: 50.23%] - LeetPush

--- 197-Rising-Temperature.sql
+++ 197-Rising-Temperature.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
 Select w1.id
 From Weather w1, Weather w2
-where dateDiff(w1.recordDate, w2.recordDate)=1 
+where dateDiff(w1.recordDate, w2.recordDate)=1 #DATEDIFF() function returns the difference between two dates, as an integer.
 And w2.temperature< w1.temperature;
